38bcd7a02f4d247649ff8fa3210d5ff3487e69db
gogopolarbear <y969956318@126.com>
2015-01-23 19:49:31 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//Yuchen Yang
+//java HelloWorld
+
+//define a class
+public class HelloWorld{
+
+//add main method
+    public static void main(String[] agrs) {
+//print the statement
+        System.out.println("##########################");
+        System.out.println("##########################");
+        System.out.println("#                        #");
+        System.out.println("#   Hello World 2015!!   #");
+        System.out.println("#                        #");
+        System.out.println("##########################");
+        System.out.println("##########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
