f96ab4975986ad6333ee41b1c8d99c714a94025b
gogopolarbear <y969956318@126.com>
2015-01-27 23:24:58 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Yuchen Yang
+//hw01
+//WelcomeClass.java
+
+//review those material in lab1
+
+//define a class
+public class WelcomeClass{
+    
+    
+//add main method  
+    public static void main(String[] agrs) {
+        
+//print the statements
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-Y--U--Y--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Last semester we choir went to New York"); 
+        System.out.println("and gave a great performance in the Carnegie Hall.");
+        
+//end of the program       
+    }
+}
